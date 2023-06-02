0e37546637cfba56382a57d833be45909312dce7
frizygartian <frizygartian08@gmail.com>
2023-06-02 17:18:36 +0700

    project akhir orbit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "swiper/css";
+import "swiper/css/pagination";
+import 'aos/dist/aos.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Routing from "./routes/Routing";
+
+function App() {
+  return (
+    <div className="App">
+      <Routing/>
+    </div>
+  );
+}
+
+export default App;
