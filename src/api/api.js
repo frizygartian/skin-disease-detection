4bc03ecdee29dea2a67c8504ae0e79c21b7899aa
Frizy Gartian <86996821+frizygartian@users.noreply.github.com>
2023-06-05 18:58:12 +0700

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://2454-119-10-182-22.ngrok-free.app'
+    baseURL: 'https://2e8d-202-80-219-186.ngrok-free.app'
 })
